4805736fb778672dd997546e4bbce99f730c8312
Raissa <raissa.santos171@etec.sp.gov.br>
2025-09-03 11:44:33 -0300

    Update SwitchAlinhado.java

--- src/logicaexercicios/SwitchAlinhado.java
+++ src/logicaexercicios/SwitchAlinhado.java
@@ -1,4 +1,27 @@
 package logicaexercicios;
 
 public class SwitchAlinhado {
+     public static void main(String[] args) {
+        int categoria = 1;
+        int opcao = 2;
+
+        switch (categoria){
+            case 1 -> {
+                System.out.println("Categoria : Matemática");
+                switch (opcao) {
+                    case 1 -> System.out.println("Soma");
+                    case 2 -> System.out.println("Subtracao");
+                }
+            }
+            case 2-> {
+                System.out.println("Categoria : Texto");
+                switch (opcao){
+                    case 1 -> System.out.println("Contar letras");
+                    case 2 -> System.out.println("Inverter string");
+                }
+            }
+
+        }
+
+    }
 }
