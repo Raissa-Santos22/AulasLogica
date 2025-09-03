648aaeeb2285d5c4661bf7297ca2e3f789f6911c
Raissa <raissa.santos171@etec.sp.gov.br>
2025-09-03 11:42:45 -0300

    Update WhileSimples.java

--- src/logicaexercicios/WhileSimples.java
+++ src/logicaexercicios/WhileSimples.java
@@ -1,4 +1,11 @@
 package logicaexercicios;
 
 public class WhileSimples {
+   public static void main(String[] args) {
+        int i = 1;
+        while(i <= 5){
+            System.out.println("Contagem : " + i);
+            i++;
+        }
+    }
 }
