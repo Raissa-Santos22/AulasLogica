bc888282bf3fdceb5cf96e47f1e091aca43414c6
Raissa <raissa.santos171@etec.sp.gov.br>
2025-09-03 11:43:22 -0300

    Update WhileAlinhado.java

--- src/logicaexercicios/WhileAlinhado.java
+++ src/logicaexercicios/WhileAlinhado.java
@@ -2,3 +2,15 @@ package logicaexercicios;
 
 public class WhileAlinhado {
 }
+  public static void main(String[] args) {
+        int i = 1;
+        while (i <= 3){
+            int j = 1;
+            while(j <= 3){
+                System.out.println(i * j + "");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
